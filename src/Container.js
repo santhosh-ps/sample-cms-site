737c671286181a5eb55bcc1b5f923650ada31f36
santhosh-ps <pssanthosh13@gmail.com>
2022-03-04 08:05:24 +0530

    update title
    update title and add link to blog post

--- src/Container.js
+++ src/Container.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import gql from 'graphql-tag';
-import { useSubscription } from '@apollo/client';
-import { Cat } from './Cat';
+import React from "react";
+import gql from "graphql-tag";
+import { useSubscription } from "@apollo/client";
+import { Cat } from "./Cat";
 
 const SUBSCRIBE_WEBCONF = gql`
   subscription MyQuery {
     websites {
-        styles
-        content
+      styles
+      content
     }
   }
 `;
 
 function Container() {
   const { loading, error, data } = useSubscription(SUBSCRIBE_WEBCONF);
-  console.log(data);
   if (loading) return <p>Loading...</p>;
   if (error || !data.websites.length) return <p>Error!(</p>;
   return (
-    <div style={{ ...(data.websites[0]?.styles || {})}}>
-      <h1> The Sample Container</h1>
-      <Cat catId={data.websites[0]?.content?.catId}/>
+    <div style={{ ...(data.websites[0]?.styles || {}) }}>
+      <h1>
+        <a
+          href="https://letsprogramit.com/posts/actions-on-google/#demo-after-the-build"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Update Me With Actions On Google &#128512;
+        </a>
+      </h1>
+      <Cat catId={data.websites[0]?.content?.catId} />
     </div>
   );
 }
